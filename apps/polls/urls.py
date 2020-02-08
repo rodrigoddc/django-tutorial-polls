9fac15a8f11cc7972581ba2d9820071e0071bef3
Rodrigo Delfino de Carvalho <rodrigoddc1@gmail.com>
2020-02-08 15:24:13 -0300

    part 4 - Django CBV

--- apps/polls/urls.py
+++ apps/polls/urls.py
@@ -16,15 +16,25 @@ Including another URLconf
 from django.urls import path
 from . import views
 
-app_name='polls'
-
+app_name = 'polls'
 urlpatterns = [
-    # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /polls/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    # Django CBV
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+
     # ex: /polls/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
+
+    # # Orientado à função
+    # # ex: /polls/
+    # path('', views.index, name='index'),
+
+    # # ex: /polls/5/
+    # # the 'name' value as called by the {% url %} template tag
+    # # added the word 'specifics'
+    # path('specifcs/<int:question_id>/', views.detail, name='detail'),
+
+    # # ex: /polls/5/results/
+    # path('<int:question_id>/results/', views.results, name='results'),
 ]
--- apps/polls/views.py
+++ apps/polls/views.py
@@ -1,29 +1,90 @@
 # Create your views here.
-from django.http import HttpResponse
+from django.db.models import F
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
-from apps.polls.models import Question
+from django.urls import reverse
+from django.views.generic import ListView, DetailView
+
+from apps.polls.models import Question, Choice
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(ListView):
+    # Django CBV
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
+
+# # Orientado à função
+# def index(request):
+#     """ :returns last 5 Questions objects
+#         filter by pub_date
+#     """
+#
+#     latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#     context = {
+#         'latest_question_list': latest_question_list
+#     }
+#     return render(request, 'polls/index.html', context)
 
 
-def detail(request, question_id):
-    # try:
-    #     question = Question.objects.get(pk=question_id)
-    # except Question.DoesNotExist:
-    #     raise Http404("Question does not exist")
-    # return render(request, 'polls/detail.html', {'question': question})
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(DetailView):
+    # Django CBV
+    model = Question
+    template_name = 'polls/detail.html'
+
+# # Orientado à função
+# def detail(request, question_id):
+#     # try:
+#     #     question = Question.objects.get(pk=question_id)
+#     # except Question.DoesNotExist:
+#     #     raise Http404("Question does not exist")
+#     # return render(request, 'polls/detail.html', {'question': question})
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/detail.html', {'question': question})
 
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+class ResultsView(DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+# # Orientado à função
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {'question': question})
 
 
+# # Orientado à função
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+
+        # O código da nossa view vote() tem um pequeno problema. Ele primeiro pega o
+        # objeto de selected_choice do banco de dados, calcula o novo valor de votes
+        # e os salva novamente. Se dois usuários do seu site tentarem votar ao mesmo
+        # tempo: o mesmo valor, digamos 42, será obtido para votes. Então, para ambos
+        # usuários, o novo valor 43 é calculado e salvo, quando o valor esperado
+        # seria 44. Isto é chamado de condição de concorrência. Se você estiver
+        # interessado, pode ler Avoiding race conditions using F() para aprender como
+        # resolver este problema:
+        # selected_choice.votes = F('votes')+1
+
+        # ou, reduzindo querys sem precisar do get() e save()
+        # selected_choice.update(votes=F('votes') + 1)
+
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
