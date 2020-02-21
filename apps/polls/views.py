faab76aa6e078d374252708cc730d4a4982e7f11
Rodrigo Delfino de Carvalho <rodrigoddc1@gmail.com>
2020-02-21 12:33:23 -0300

    (done) part 5 - TestCases

--- apps/polls/views.py
+++ apps/polls/views.py
@@ -3,6 +3,7 @@ from django.db.models import F
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
 from django.views.generic import ListView, DetailView
 
 from apps.polls.models import Question, Choice
@@ -14,8 +15,14 @@ class IndexView(ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
+
 
 # # Orientado à função
 # def index(request):
@@ -34,6 +41,12 @@ class DetailView(DetailView):
     # Django CBV
     model = Question
     template_name = 'polls/detail.html'
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 # # Orientado à função
 # def detail(request, question_id):
