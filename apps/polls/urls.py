f7eb70721fbec61e0c1e44c163e2ce2ba49a5855
Rodrigo Delfino de Carvalho <rodrigoddc1@gmail.com>
2020-02-21 20:50:56 -0300

    (done) part 7 - serving static files in production

--- apps/polls/urls.py
+++ apps/polls/urls.py
@@ -18,7 +18,7 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    # Django CBV
+    # \\Django CBV
     path('', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
